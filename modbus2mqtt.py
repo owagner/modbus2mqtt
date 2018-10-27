0ffd123ecd51a78ba259f405506e54498154378c
Max Brueggemann <mail@maxbrueggemann.de>
2018-10-27 11:13:40 +0000

    Revert "Revert "Addded command line argument for setting break time in main loop.""
    This reverts commit 45000be99984dc4439c7d1fa21835411d9c02b3b.

--- modbus2mqtt.py
+++ modbus2mqtt.py
@@ -113,6 +113,7 @@ class Poller:
         self.device=None
         self.disabled=False
         self.failcounter=0
+        self.oldData=[]
 
         for myDev in deviceList:
             if myDev.name == self.topic:
@@ -143,7 +144,7 @@ class Poller:
 
     def poll(self):
 #        try:
-        try:
+        if True:
             result = None
             failed = True
             if self.functioncode == 3:
@@ -170,16 +171,28 @@ class Poller:
                     failed = False
 
             if not failed:
-                for ref in self.readableReferences:
-                    ref.checkPublish(data,self.topic)
+                changed=False
+                if len(data)>len(self.oldData):
+                    for d in data:
+                        self.oldData.append(d)
+
+                for x in range(len(data)):
+                    if not self.oldData[x] == data[x]:
+                        self.oldData[x]=data[x]
+                        changed=True
+                        break
+
+                if changed:
+                    for ref in self.readableReferences:
+                        ref.checkPublish(data,self.topic)
             
             if args.autoremove:
                 self.failCount(failed)
 
-        except:
-            print("Error talking to slave device "+str(self.slaveid)+" (maybe CRC error or timeout)")
-            if args.autoremove:
-                self.failCount(failed)
+        #except:
+         #   print("Error talking to slave device "+str(self.slaveid)+" (maybe CRC error or timeout)")
+          #  if args.autoremove:
+           #     self.failCount(failed)
 
 
     def checkPoll(self):
